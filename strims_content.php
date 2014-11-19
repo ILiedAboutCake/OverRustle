b91dde7de1f9f44730c57f25c84d6dd00ba707a8
Hayk Saakian <fakest.id.ever@gmail.com>
2014-11-19 02:46:48 -0800

    start thumbnails

--- strims_content.php
+++ strims_content.php
@@ -1,10 +1,30 @@
 <br />
     <script type="text/javascript" src="//api.OverRustle.com/socket.io/socket.io.js"></script>
-
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js"></script>
 <div class="container">
 <div class="row">
+  <div class="col-md-4"></div>  
+  <div id="status"></div>
   <div class="col-md-4"></div>
-  <script type="text/javascript">
+</div>
+
+<h1 align="center" style="color: #FFFFFF;">See what <span id="viewercount"></span> rustlers are watching!</h1>
+<div id="strims">
+</div>
+<br />
+<div style="text-align: center; color: #FFFFFF;">
+  JSON API can be found at: <a href="http://overrustle.com:9998/api">http://overrustle.com:9998/api</a><br />
+  Donate: 
+    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6TUMKXJ23YGQG">Paypal</a>, 
+    <a href="bitcoin:14j82o37d8AShtVP5LydYCptVxwMhbu4Z1">Bitcoin</a>, 
+    <a href="dogecoin:DS6JdMnt96CVXEXZ2LNdjKq6kmcSD7mC88">Dogecoin</a>, 
+    <a href="https://www.linode.com/?r=57232eb9908d0f24a8907e61106c88f475248ac7">Linode.com Referral</a><Br />
+  <a href="https://github.com/ILiedAboutCake/OverRustle">Github</a> - ILiedAboutCake 2014
+</div>
+    </div><!-- /.container -->
+
+
+<script type="text/javascript">
   // client side code
   var socket = io('http://api.overrustle.com');
 
@@ -14,10 +34,23 @@
     var strims = api_data["streams"]
     $('#strims').html('')
     new_html = ""
-    $.each(strims, function(strim, viewers){
-      new_html = new_html + "<tr><td><a target='_blank' href='"+ strim +"'>"+ strim +"</a></td><td>~"+viewers+"</td></tr>"
+    var counter = 0
+    var source   = $("#card").html();
+    Mustache.parse(source)
+    var card_template = function (data) {
+      return Mustache.render(source, data);
+    }
+    $("#strims").html("")
+    $.each(strims, function(strim, viewercount){
+      var metadata = api_data.metadata[api_data.metaindex[strim]]
+      metadata.strim = strim
+      metadata.viewercount = viewercount
+      if(counter % 4 == 0){
+        $('#strims').append("<div class='row'></div>")
+      }
+      counter += 1; 
+      $('#strims > .row').last().append(card_template(metadata))
     });
-    $('#strims').html(new_html)
     status = "<div class='label label-success col-md-4' role='alert'>Tracking server is currently online.</div>"
     $('#status').html(status)
   });
@@ -26,32 +59,18 @@
     status = "<div class='label label-warning col-md-4' role='alert'>Tracking server is currently offline.</div>"
     $('#status').html(status)
   })
-  </script>
-  
-<div id="status">
-</div>
+</script>
 
-  <div class="col-md-4"></div>
-</div>
-
-<h1 align="center" style="color: #FFFFFF;">See what <span id="viewercount"></span> rustlers are watching!</h1>
-<table class="table" style="color: #FFFFFF;">
-  <thead>
-    <tr>
-      <th>Stream Link</th>
-      <th>Viewer Count</th>
-    </tr>
-  </thead>
-  <tbody id="strims"></tbody>
-</table>
-<br />
-<div style="text-align: center; color: #FFFFFF;">
-  JSON API can be found at: <a href="http://api.overrustle.com/api">http://api.overrustle.com/api</a><br />
-  Donate: 
-    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6TUMKXJ23YGQG">Paypal</a>, 
-    <a href="bitcoin:14j82o37d8AShtVP5LydYCptVxwMhbu4Z1">Bitcoin</a>, 
-    <a href="dogecoin:DS6JdMnt96CVXEXZ2LNdjKq6kmcSD7mC88">Dogecoin</a>, 
-    <a href="https://www.linode.com/?r=57232eb9908d0f24a8907e61106c88f475248ac7">Linode.com Referral</a><Br />
-  <a href="https://github.com/ILiedAboutCake/OverRustle">Github</a> - ILiedAboutCake 2014
-</div>
-    </div><!-- /.container -->
+<script id="card" type="x-tmpl-mustache">
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="{{image_url}}" alt="{{channel}} on {{platform}} ">
+      <div class="caption">
+        <div>
+          {{viewercount}} <span class="glyphicon glyphicon-user" aria-hidden="true"></span> 
+          <a href="{{strim}}">{{channel}} on {{platform}}</a>
+        </div>
+      </div>
+    </div>
+  </div>  
+</script>
