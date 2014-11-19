090689321cb179288c3e2d8202468b94e3e0f7fe
ILiedAboutCake <iliedaboutthecake@gmail.com>
2014-11-19 01:25:08 -0500

    Added autist list
    look I used more PHP, stay mad?

---
+++ autistlist.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="favicon.ico">
+    <style>
+      body{ background-color: white !important; }
+    </style>
+
+    <title>OverRustle.com - Autist List</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/overrustle.css" rel="stylesheet">
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <?php include ('navbar.php'); ?>
+
+    <iframe src="http://overrustlelogs.net/Destinygg%20chatlog/<?php echo date("F") . "%20" . date("Y"); ?>/userlogs/?C=S;O=D" width="100%" height="100%" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
