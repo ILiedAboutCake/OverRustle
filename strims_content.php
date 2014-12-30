3ea8846e5e76eee62c6a3bdecc9fcbafde986857
Hayk Saakian <fakest.id.ever@gmail.com>
2014-12-29 21:42:26 -0800

    indent, and add a class to thumbnails

--- strims_content.php
+++ strims_content.php
@@ -2,26 +2,26 @@
     <script type="text/javascript" src="//api.OverRustle.com/socket.io/socket.io.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.min.js"></script>
 <div class="container">
-<div class="row">
-  <div class="col-md-4"></div>  
-  <div id="status"></div>
-  <div class="col-md-4"></div>
-</div>
+  <div class="row">
+    <div class="col-md-4"></div>  
+    <div id="status"></div>
+    <div class="col-md-4"></div>
+  </div>
 
-<h1 align="center" style="color: #FFFFFF;">See what <span id="viewercount"></span> rustlers are watching!</h1>
-<div id="strims">
-</div>
-<br />
-<div style="text-align: center; color: #FFFFFF;">
-  JSON API can be found at: <a href="http://overrustle.com:9998/api">http://overrustle.com:9998/api</a><br />
-  Donate: 
-    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6TUMKXJ23YGQG">Paypal</a>, 
-    <a href="bitcoin:14j82o37d8AShtVP5LydYCptVxwMhbu4Z1">Bitcoin</a>, 
-    <a href="dogecoin:DS6JdMnt96CVXEXZ2LNdjKq6kmcSD7mC88">Dogecoin</a>, 
-    <a href="https://www.linode.com/?r=57232eb9908d0f24a8907e61106c88f475248ac7">Linode.com Referral</a><Br />
-  <a href="https://github.com/ILiedAboutCake/OverRustle">Github</a> - ILiedAboutCake 2014
-</div>
-    </div><!-- /.container -->
+  <h1 align="center" style="color: #FFFFFF;">See what <span id="viewercount"></span> rustlers are watching!</h1>
+  <div id="strims">
+  </div>
+  <br />
+  <div style="text-align: center; color: #FFFFFF;">
+    JSON API can be found at: <a href="http://overrustle.com:9998/api">http://overrustle.com:9998/api</a><br />
+    Donate: 
+      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6TUMKXJ23YGQG">Paypal</a>, 
+      <a href="bitcoin:14j82o37d8AShtVP5LydYCptVxwMhbu4Z1">Bitcoin</a>, 
+      <a href="dogecoin:DS6JdMnt96CVXEXZ2LNdjKq6kmcSD7mC88">Dogecoin</a>, 
+      <a href="https://www.linode.com/?r=57232eb9908d0f24a8907e61106c88f475248ac7">Linode.com Referral</a><Br />
+    <a href="https://github.com/ILiedAboutCake/OverRustle">Github</a> - ILiedAboutCake 2014
+  </div>
+</div><!-- /.container -->
 
 
 <script type="text/javascript">
@@ -87,7 +87,7 @@
     <div class="thumbnail">
       {{#live}}
       <a href="{{strim}}">
-        <img src="{{image_url}}" alt="{{channel}} on {{platform}} ">
+        <img class="stream-thumbnail" src="{{image_url}}" alt="{{channel}} on {{platform}} ">
       </a>
       {{/live}}
       <div class="caption">
