afc88f60cfb7bc1956738eb868ad698a236d4cdb
Hayk Saakian <fakest.id.ever@gmail.com>
2015-01-09 20:51:44 -0800

    fix jquery reference

--- strims.php
+++ strims.php
@@ -39,7 +39,7 @@ require_once 'session.php';
     include('strims_content.php');
      ?>
 
-    <script src="js/jquery-1.11.2.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/ie10-viewport-bug-workaround.js"></script>
   </body>
