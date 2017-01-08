d3ace048484436b39ed1ce0e7511beac5f79d909
ILiedAboutCake <iliedaboutthecake@gmail.com>
2017-01-08 18:23:53 -0500

    Cleanup endpoints and list, add facebook live

--- js/constants.js
+++ js/constants.js
@@ -1,62 +1,62 @@
 var SERVICES = {
-  "twitch": {
-    display_name: "Twitch",
-    chat: true
-  },
-  "twitch-vod": {
-    display_name: "Twitch VOD",
-    chat: true
-  },
-  "ustream": {
-    display_name: "Ustream",
-    chat: true
-  },
-  "streamup": {
-    display_name: "Streamup",
-    chat: false
-  },
-  "hitbox": {
-    display_name: "hitbox",
-    chat: true
-  },
-  "hitbox-vod": {
-    display_name: "hitbox VOD",
-    chat: false
-  },
-  "azubu": {
-    display_name: "Azubu",
-    chat: true
-  },
-  "vaughn": {
-    display_name: "VaughnLive",
-    chat: true
-  },
   "angelthump": {
     display_name: "AngelThump",
     chat: false
   },
-  "riven": {
-    display_name: "Riven - Destiny VOD",
+  "azubu": {
+    display_name: "azubu",
+    chat: true
+  },
+  "dailymotion": {
+    display_name: "Dailymotion",
     chat: false
   },
-  "nsfw-chaturbate": {
-    display_name: "Chaturbate (NSFW)",
+  "facebook": {
+    display_name: "Facebook",
     chat: false
   },
-  "youtube": {
-    display_name: "YouTube",
-    chat: false
+  "hitbox": {
+    display_name: "Hitbox",
+    chat: true
   },
-  "youtube-playlist": {
-    display_name: "YouTube (Playlist)",
+  "hitbox-vod": {
+    display_name: "hitbox (VOD)",
     chat: false
   },
   "mlg": {
     display_name: "MLG",
     chat: false
   },
-  "dailymotion": {
-    display_name: "Dailymotion",
+  "nsfw-chaturbate": {
+    display_name: "Chaturbate (NSFW)",
+    chat: false
+  },
+  "streamup": {
+    display_name: "StreamUp",
+    chat: false
+  },
+  "twitch": {
+    display_name: "Twitch",
+    chat: true
+  },
+  "twitch-vod": {
+    display_name: "Twitch (VOD)",
+    chat: true
+  },
+  "ustream": {
+    display_name: "Ustream",
+    chat: true
+  },
+  "vaughn": {
+    display_name: "Vaughn",
+    chat: true
+  },
+  "youtube": {
+    display_name: "YouTube",
+    chat: false
+  },
+  "youtube-playlist": {
+    display_name: "YouTube (playlist)",
     chat: false
   }
 };
--- jsx/constants.js
+++ jsx/constants.js
@@ -1,66 +1,62 @@
 var SERVICES = {
-  "twitch":{
-    display_name: "Twitch",
-    chat: true
-  },
-  "twitch-vod":{
-    display_name: "Twitch VOD",
-    chat: true
-  },
-  "ustream":{
-    display_name: "Ustream",
-    chat: true
-  },
-  "streamup":{
-    display_name: "Streamup",
-    chat: false
-  },
-  "hitbox":{
-    display_name: "hitbox",
-    chat: true
-  },
-  "hitbox-vod":{
-    display_name: "hitbox VOD",
-    chat: false
-  },
-  "azubu":{
-    display_name: "Azubu",
-    chat: true
-  },
-  "vaughn":{
-    display_name: "VaughnLive",
-    chat: true
-  },
-  "angelthump":{
+  "angelthump": {
     display_name: "AngelThump",
-    chat: false    
-  },
-  "riven":{
-    display_name: "Riven - Destiny VOD",
     chat: false
   },
-  "nsfw-chaturbate":{
-    display_name: "Chaturbate (NSFW)",
+  "azubu": {
+    display_name: "azubu",
+    chat: true
+  },
+  "dailymotion": {
+    display_name: "Dailymotion",
     chat: false
   },
-  "youtube":{
-    display_name: "YouTube",
+  "hitbox": {
+    display_name: "Hitbox",
+    chat: true
+  },
+  "hitbox-vod": {
+    display_name: "hitbox (VOD)",
     chat: false
   },
-  "youtube-playlist":{
-    display_name: "YouTube (Playlist)",
-    chat: false
-  },
-  "mlg":{
+  "mlg": {
     display_name: "MLG",
     chat: false
   },
-  "dailymotion":{
-    display_name: "Dailymotion",
+  "nsfw-chaturbate": {
+    display_name: "Chaturbate (NSFW)",
+    chat: false
+  },
+  "streamup": {
+    display_name: "StreamUp",
+    chat: false
+  },
+  "twitch": {
+    display_name: "Twitch",
+    chat: true
+  },
+  "twitch-vod": {
+    display_name: "Twitch (VOD)",
+    chat: true
+  },
+  "ustream": {
+    display_name: "Ustream",
+    chat: true
+  },
+  "vaughn": {
+    display_name: "Vaughn",
+    chat: true
+  },
+  "youtube": {
+    display_name: "YouTube",
+    chat: false
+  },
+  "youtube-playlist": {
+    display_name: "YouTube (playlist)",
     chat: false
   }
-}
+};
 module.exports = {
   "SERVICES": SERVICES,
   "TWITCH_TOKEN_URL": "https://api.twitch.tv/kraken/oauth2/token"
-}
+};
